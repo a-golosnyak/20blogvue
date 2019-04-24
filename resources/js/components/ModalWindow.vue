b2c4e56bda393bfa49ddea4b8b915c68170928b7
a-golosnyak <andrey_g.pt@mail.ru>
2019-04-24 22:57:18 +0300

    Modal window added.

---
+++ resources/js/components/ModalWindow.vue
@@ -0,0 +1,20 @@
+<template>
+    <div
+        id="modal" 
+        :class="{ show : modalOpen }"
+        >
+        <button @click="modalOpen =false" class="modal-close">&times;</button>
+        <div class="modal-content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
+
